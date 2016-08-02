bcafc936deec9861612ee394af310fcebe36b99c
Rob Herring <robh@kernel.org>
2016-08-02 16:54:52 -0500

    egl: android: remove the drm_gralloc dependency
    As the render node is used now, we don't need to retrieve the fd from
    gralloc and can just open the device directly.
    
    FIXME: This breaks existing users, so can't be upstreamed yet.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- src/egl/drivers/dri2/platform_android.c
+++ src/egl/drivers/dri2/platform_android.c
@@ -968,6 +968,7 @@ droid_add_configs_for_visuals(_EGLDriver *drv, _EGLDisplay *dpy)
 static int
 droid_open_device(struct dri2_egl_display *dri2_dpy)
 {
+#ifdef HAS_GRALLOC_DRM_HEADERS
    int fd = -1, err = -EINVAL;
 
    if (dri2_dpy->gralloc->perform)
@@ -980,6 +981,9 @@ droid_open_device(struct dri2_egl_display *dri2_dpy)
    }
 
    return (fd >= 0) ? fcntl(fd, F_DUPFD_CLOEXEC, 3) : -1;
+#else
+   return loader_open_device("/dev/dri/renderD128");
+#endif
 }
 
 /* support versions < JellyBean */
